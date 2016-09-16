ff43cdd7ca4c45cb119ecb365e11bb7f101a9999
adorechic <adorechic@gmail.com>
2016-09-16 20:57:44 +0900

    Remove unnecessary namespace

--- spec/cred_stash/repository/dynamo_db_spec.rb
+++ spec/cred_stash/repository/dynamo_db_spec.rb
@@ -1,49 +1,78 @@
 require 'spec_helper'
 
-describe CredStash::Repository do
-  describe CredStash::Repository::DynamoDB do
-    describe '#get' do
-      let(:stub_client) do
-        Aws::DynamoDB::Client.new(
-          stub_responses: { query: { items: items } }
-        )
-      end
-
-      let(:items) do
-        [{ 'key' => 'data_key', 'contents' => 'contents' }]
-      end
-
-      it 'returns item'do
-        item = described_class.new(client: stub_client).get('name')
-        expect(item.key).to eq 'data_key'
-        expect(item.contents).to eq 'contents'
-      end
-
-      context 'if item is not found' do
-        let(:items) { [] }
-
-        it 'raises error' do
-          expect{
-            described_class.new(client: stub_client).get('name')
-          }.to raise_error(CredStash::ItemNotFound)
-        end
-      end
+describe CredStash::Repository::DynamoDB do
+  describe '#get' do
+    let(:stub_client) do
+      Aws::DynamoDB::Client.new(
+        stub_responses: { query: { items: items } }
+      )
     end
 
-    describe '#put' do
-      let(:item) do
-        CredStash::Repository::Item.new(
-          name: 'name',
-          version: "%019d" % 1,
-          key: 'base64_encoded_key',
-          contents: 'base64_encoded_contents',
-          hmac: 'hmac'
-        )
+    let(:items) do
+      [{ 'key' => 'data_key', 'contents' => 'contents' }]
+    end
+
+    it 'returns item'do
+      item = described_class.new(client: stub_client).get('name')
+      expect(item.key).to eq 'data_key'
+      expect(item.contents).to eq 'contents'
+    end
+
+    context 'if item is not found' do
+      let(:items) { [] }
+
+      it 'raises error' do
+        expect{
+          described_class.new(client: stub_client).get('name')
+        }.to raise_error(CredStash::ItemNotFound)
+      end
+    end
+  end
+
+  describe '#put' do
+    let(:item) do
+      CredStash::Repository::Item.new(
+        name: 'name',
+        version: "%019d" % 1,
+        key: 'base64_encoded_key',
+        contents: 'base64_encoded_contents',
+        hmac: 'hmac'
+      )
+    end
+
+    it 'puts item to DynamoDB' do
+      put_params = {
+        table_name:  'credential-store',
+        item: {
+          name: item.name,
+          version: item.version,
+          key: item.key,
+          contents: item.contents,
+          hmac: item.hmac
+        },
+        condition_expression: "attribute_not_exists(#name)",
+        expression_attribute_names: { "#name" => "name" },
+      }
+
+      stub_client = double
+      expect(stub_client).to receive(:put_item).with(put_params)
+      described_class.new(client: stub_client).put(item)
+    end
+
+    context 'with table_name option' do
+      before do
+        CredStash.configure do |config|
+          config.table_name = 'awesome_table'
+        end
+      end
+
+      after do
+        CredStash.config.reset!
       end
 
       it 'puts item to DynamoDB' do
         put_params = {
-          table_name:  'credential-store',
+          table_name: 'awesome_table',
           item: {
             name: item.name,
             version: item.version,
@@ -59,94 +88,63 @@ describe CredStash::Repository do
         expect(stub_client).to receive(:put_item).with(put_params)
         described_class.new(client: stub_client).put(item)
       end
+    end
+  end
 
-      context 'with table_name option' do
-        before do
-          CredStash.configure do |config|
-            config.table_name = 'awesome_table'
-          end
-        end
-
-        after do
-          CredStash.config.reset!
-        end
-
-        it 'puts item to DynamoDB' do
-          put_params = {
-            table_name: 'awesome_table',
-            item: {
-              name: item.name,
-              version: item.version,
-              key: item.key,
-              contents: item.contents,
-              hmac: item.hmac
-            },
-            condition_expression: "attribute_not_exists(#name)",
-            expression_attribute_names: { "#name" => "name" },
-          }
-
-          stub_client = double
-          expect(stub_client).to receive(:put_item).with(put_params)
-          described_class.new(client: stub_client).put(item)
-        end
-      end
+  describe '#list' do
+    let(:stub_client) do
+      Aws::DynamoDB::Client.new(
+        stub_responses: { scan: { items: items } }
+      )
     end
 
-    describe '#list' do
-      let(:stub_client) do
-        Aws::DynamoDB::Client.new(
-          stub_responses: { scan: { items: items } }
-        )
-      end
-
-      let(:items) do
-        [{ 'name' => 'name', 'version' => '0000001' }]
-      end
-
-      it 'returns items' do
-        items = described_class.new(client: stub_client).list
-        expect(items.size).to eq 1
-
-        item = items.first
-        expect(item.name).to eq 'name'
-        expect(item.version).to eq '0000001'
-      end
+    let(:items) do
+      [{ 'name' => 'name', 'version' => '0000001' }]
     end
 
-    describe '#select' do
-      let(:stub_client) do
-        Aws::DynamoDB::Client.new(
-          stub_responses: { query: { items: items } }
-        )
-      end
+    it 'returns items' do
+      items = described_class.new(client: stub_client).list
+      expect(items.size).to eq 1
 
-      let(:items) do
-        [{ 'name' => 'name', 'version' => 'version' }]
-      end
+      item = items.first
+      expect(item.name).to eq 'name'
+      expect(item.version).to eq '0000001'
+    end
+  end
 
-      it 'returns item' do
-        items = described_class.new(client: stub_client).select('name')
-        expect(items.size).to eq 1
-        expect(items.first.name).to eq 'name'
-      end
+  describe '#select' do
+    let(:stub_client) do
+      Aws::DynamoDB::Client.new(
+        stub_responses: { query: { items: items } }
+      )
     end
 
-    describe '#delete' do
-      let(:item) do
-        CredStash::Repository::Item.new(name: 'name', version: '0001')
-      end
+    let(:items) do
+      [{ 'name' => 'name', 'version' => 'version' }]
+    end
 
-      it 'deletes from DynamoDB' do
-        stub_client = double
-        expect(stub_client).to receive(:delete_item).with(
-                                 table_name:  'credential-store',
-                                 key: {
-                                   name: 'name',
-                                   version: '0001'
-                                 }
-                               )
-        described_class.new(client: stub_client).delete(item)
-      end
+    it 'returns item' do
+      items = described_class.new(client: stub_client).select('name')
+      expect(items.size).to eq 1
+      expect(items.first.name).to eq 'name'
+    end
+  end
+
+  describe '#delete' do
+    let(:item) do
+      CredStash::Repository::Item.new(name: 'name', version: '0001')
+    end
+
+    it 'deletes from DynamoDB' do
+      stub_client = double
+      expect(stub_client).to receive(:delete_item).with(
+                               table_name:  'credential-store',
+                               key: {
+                                 name: 'name',
+                                 version: '0001'
+                               }
+                             )
+      described_class.new(client: stub_client).delete(item)
     end
   end
 end
