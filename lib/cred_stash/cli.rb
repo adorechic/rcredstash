9655813b8926d130c1dd44b0d37a28ce3a855fb4
Tetsuya OHIRA <tetsuya.ohira@starttoday.jp>
2017-09-13 13:05:57 +0900

    adding `--kms-key-id` option in `put`

--- lib/cred_stash/cli.rb
+++ lib/cred_stash/cli.rb
@@ -8,9 +8,11 @@ module CredStash
     end
 
     desc "put [key name]", "Put a value for key name"
+    option :kms_key_id , :desc => "the KMS key-id of the master key to use. Defaults to alias/credstash"
     def put(name)
       value = Readline.readline("secret value> ")
-      CredStash.put(name, value)
+      kms_key_id = options[:kms_key_id] if options[:kms_key_id]
+      CredStash.put(name, value, kms_key_id: kms_key_id)
       puts "#{name} has stored."
     end
 
